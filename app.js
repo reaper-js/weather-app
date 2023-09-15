0d067745d3c67ebea3d64887b6618d6bd86b119e
Jatin Saini <jatinsaini191@gmail.com>
2023-09-15 17:08:05 +0530

    creating utils

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const request = require('request')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
-const url = 'http://api.weatherstack.com/current?access_key=f94d5ce579ecfc906b47cf0b322158bc&query=37.8267,-122.4233'
 
-request({url: url, json: true}, (error, response) => {
-    //const data = JSON.parse(response.body)
-    //console.log(data.current)
-    console.log('it is currently ' + response.body.current.temperature + ' degrees out there. It feels like ' + response.body.current.feelslike + 'degrees out.')
+
+geocode('Alwar', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
 
 
-const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYm9ic2xlZCIsImEiOiJjbG1rYXN0amYwMHV1Mmp0YmhlOXh4d2JhIn0.0bb-6GECLjKjOxMSNLScng&limit=1'
 
-request({url: url1, json: true}, (error, response) => {
-    //console.log(response.body)
-    const coords = response.body.features.center
-    console.log('Lat = ' + coords[0] + ' Long = ' + coords[1])
-})
+forecast(27.564631,76.613185, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
---
+++ utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (lati, longi, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f94d5ce579ecfc906b47cf0b322158bc&query='+ lati+','+ longi +'&units=m'
+    
+    request({url: url, json: true}, (error, response)=> {
+        if(error){
+            callback('Unable to connect to weather service', undefined)
+        }else if(response.body.error){
+            callback('Unable to find the location', undefined)
+        }else{
+            callback(undefined, 'The temperature is '+ response.body.current.temperature + ' degrees. And it feels like ' + response.body.current.feelslike + ' out there.')
+        }
+    })
+
+}
+
+module.exports = forecast
